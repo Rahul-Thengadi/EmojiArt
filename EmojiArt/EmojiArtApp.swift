a97a33c6138d0684ff0d61620cff2886f621d522
Rahul Thengadi <thengadir@gmail.com>
2021-02-25 23:45:56 +0530

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by RaHuL on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
